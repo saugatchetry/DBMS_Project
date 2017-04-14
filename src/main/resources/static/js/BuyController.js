37af428e951c167e35ed228fe6eb098a866cb580
Nishantagarwal <Nishantagarwal@10.228.0.175>
2017-04-14 19:58:34 -0400

    Image load

--- src/main/resources/static/js/BuyController.js
+++ src/main/resources/static/js/BuyController.js
@@ -1,3 +1,11 @@
-app.controller('BuyController', ['$scope', '$http', function($scope, $http) {
-
+angular.module('myApp').controller('BuyController', ['$scope', '$http', function($scope, $http) {
+	
+	$scope.loadImage = function(){
+		$http({
+			method : "GET",
+			url : "downloadImage",	       
+		}).then(function(response) {			
+			$scope.image = response.data;
+		});
+	}
 }]);
