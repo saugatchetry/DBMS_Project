7ebbdc96560873cb0e46eb2e55fba8ee63bdaa62
Nishantagarwal <Nishantagarwal@10.228.0.175>
2017-04-15 00:41:16 -0400

    Local changes before taking Anitha code

--- src/main/resources/static/js/SellController.js
+++ src/main/resources/static/js/SellController.js
@@ -1,3 +1,22 @@
 angular.module('myApp').controller('SellController', ['$scope', '$http', function($scope, $http) {
+	$scope.getCityNames = function(){
+		$http({
+	        method : "GET",
+	        url : "getCity",	     
+	    }).then(function(response) {
+        	$scope.cities = response.data;
+        	$scope.selectedCity = $scope.cities[0];
+    	});		
+	};
+	
+	$scope.propertyProp = {
+			'WoodenFlooring' : 0,
+			'Carpet' : 0,
+			'Parking' : 0,
+			'PetFriendly' : 0,
+			'Pool' : 0,
+			'WaterFront' : 0,
+			'View' : 0
+	};
 	
 }]);
